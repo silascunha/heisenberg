1bc8e5618be0493edb5a1f123c2bd9d9072e83f5
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2022-05-09 20:06:37 -0300

    Crfeat: criação do DTO OrientaçcaoOutput para saída de orientações

--- src/main/java/br/dev/silascunha/heisenberg/dto/OrientacaoOutput.java
+++ src/main/java/br/dev/silascunha/heisenberg/dto/OrientacaoOutput.java
@@ -1,16 +1,21 @@
 package br.dev.silascunha.heisenberg.dto;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import lombok.*;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class OrientacaoInput implements Serializable {
+public class OrientacaoOutput implements Serializable {
 
+    private Integer id;
     private Integer idTipo;
+    private String nomeTipo;
     private Integer idExame;
     private String descricao;
 
